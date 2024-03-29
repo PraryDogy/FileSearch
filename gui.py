521cec7591224bc5c482f6cacdf1e86d8283bf16
Evgeny Loshkarev <evlosh@gmail.com>
2024-03-29 13:16:27 +0300

    ref

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-import json
 import os
 import subprocess
 import sys
@@ -6,14 +5,15 @@ from functools import partial
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QGuiApplication
-from PyQt6.QtWidgets import (QApplication, QFileDialog, QLineEdit, QMessageBox,
-                             QPushButton, QVBoxLayout, QWidget, QHBoxLayout, QLabel)
+from PyQt6.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
+                             QLineEdit, QMessageBox, QPushButton, QVBoxLayout,
+                             QWidget)
 
-from cfg import Cfg
 from catalog_mirgrate import CatalogMigrateThread
-from reveal_files import RevealFiles
-from catalog_update import CatalogUpdateThread
 from catalog_search import catalog_search_file
+from catalog_update import CatalogUpdateThread
+from cfg import Cfg
+from reveal_files import RevealFiles
 
 
 class SearchApp(QWidget):
@@ -27,7 +27,7 @@ class SearchApp(QWidget):
         self.init_ui()
         self.setFocus()
         self.center()
-        self.catalog_check()
+        self.error_check()
 
     def init_ui(self):
         self.v_layout = QVBoxLayout()
@@ -43,7 +43,7 @@ class SearchApp(QWidget):
         self.h_layout.addWidget(self.browse_lbl)
 
         self.update_btn = QPushButton("Обновить базу данных", self)
-        self.update_btn.clicked.connect(self.update_db)
+        self.update_btn.clicked.connect(self.update_btn_cmd)
         
         self.input_text = QLineEdit(self)
         self.input_text.setPlaceholderText("Вставьте артикул или ссылку")
@@ -57,47 +57,49 @@ class SearchApp(QWidget):
         
         self.setLayout(self.v_layout)
 
-
-    def catalog_check(self):
+    def error_check(self):
         if Cfg.first_load:
-            self.disable_gui()
+            self.gui_switch(setDisabled=True)
+            self.choose_catalog()
 
-            # new_dir = QFileDialog.getExistingDirectory(self)
+    def choose_catalog(self):
+        new_dir = QFileDialog.getExistingDirectory(self)
 
-            # if new_dir:
+        if new_dir:
+                old_dir = Cfg.images_dir
+                Cfg.images_dir = new_dir
+                Cfg.first_load = False
+                Cfg.write_cfg_json_file()
 
-            #     old_dir = Cfg.images_dir
+                self.migrate_thread = CatalogMigrateThread(old_dir, new_dir)
+                self.migrate_thread.finished.connect(self.finalize_choose_catalog)
+                self.migrate_thread.start()
 
-            #     Cfg.images_dir = new_dir
-            #     Cfg.first_load = False
-            #     Cfg.write_cfg_json_file()
+    def finalize_choose_catalog(self):
+        self.gui_switch(setDisabled=False)
+        self.browse_lbl.setText(Cfg.images_dir)
 
-            # self.migrate_thread = CatalogMigrateThread(old_dir, new_dir)
-            # self.migrate_thread.finished.connect(self.finalize_migrate)
-            # self.setDisabled(True)
-            # self.migrate_thread.start()
-
-    def disable_gui(self):
+    def gui_switch(self, setDisabled: bool):
         self.remove_article_btns()
         for i in (self.update_btn, self.input_text, self.search_button):
-            i.setDisabled(True)
+            i.setDisabled(setDisabled)
 
     def remove_article_btns(self):
         for i in self.btns:
             try:
+                i: QPushButton
                 i.deleteLater()
             except Exception:
                 pass
         self.btns.clear()
 
-    def update_db(self):
+    def update_btn_cmd(self):
         self.update_btn.setText("Подождите...")
         self.t1 = CatalogUpdateThread()
-        self.t1.finished.connect(self.finalize_update_db)
+        self.t1.finished.connect(self.finalize_update_btn_cmd)
         self.t1.start()
 
-    def finalize_update_db(self):
-        self.setDisabled(False)
+    def finalize_update_btn_cmd(self):
         self.update_btn.setText("Обновить базу данных")
     
     def warning(self):
@@ -112,76 +114,42 @@ class SearchApp(QWidget):
             self.warning()
             return
 
-        for i in self.btns:
-            try:
-                i.deleteLater()
-            except RuntimeError:
-                pass
-
+        self.remove_article_btns()
         self.setFocus()
         text: str = self.input_text.text()
-
-        if "miuz.ru" in text:
-            text = text.strip("/").split("/")[-1]
         
         if not text or len(text) < 5:
             return
+
+        if "miuz.ru" in text:
+            text = text.strip("/").split("/")[-1]
 
         res: dict = catalog_search_file(text)
 
         if res:
             for name, src in res:
                 btn = QPushButton(name, self)
-                btn.clicked.connect(partial(self.open_btn, src))
+                btn.clicked.connect(partial(self.article_btn_cmd, src))
                 btn.setStyleSheet("text-align:left")
                 self.v_layout.addWidget(btn)
-
                 self.btns.append(btn)
 
-        self.setFocus()
-
-    def open_btn(self, path: str):
+    def article_btn_cmd(self, path: str):
         print("open", path)
         subprocess.run(["open", "-R", path])
 
-        if not os.path.exists(Cfg.data["catalog"]):
+        if not os.path.exists(Cfg.images_dir):
             self.warning()
-            return
 
         # RevealFiles(files_list=[path])
 
     def center(self):
-        # Получаем список экранов
         screens = QGuiApplication.screens()
-
-        # Получаем геометрию первого экрана
         screen_geometry = screens[0].availableGeometry()
-
-        # Получаем размеры окна
         window_geometry = self.frameGeometry()
-
-        # Центрируем окно по центру экрана
         window_geometry.moveCenter(screen_geometry.center())
         self.move(window_geometry.topLeft())
         
-    def choose_catalog(self):
-        new_dir = QFileDialog.getExistingDirectory(self)
-
-        if new_dir:
-            old_dir = Cfg.data["catalog"]
-            Cfg.data["catalog"] = new_dir
-            self.browse_lbl.setText(new_dir)
-
-            with open(Cfg.cfg_json_file, "w", encoding="utf=8") as file:
-                json.dump(Cfg.data, file, ensure_ascii=False, indent=2)
-
-            self.migrate_thread = CatalogMigrateThread(old_dir, new_dir)
-            self.migrate_thread.finished.connect(self.finalize_migrate)
-            self.setDisabled(True)
-            self.migrate_thread.start()
-
-    def finalize_migrate(self):
-        self.setDisabled(False)
 
 if __name__ == "__main__":
     Cfg.check()
@@ -202,10 +170,9 @@ if __name__ == "__main__":
         os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
 
     app = QApplication(sys.argv)
-    
-    # Установка системного стиля для macOS
     app.setStyle('macos')
     
     window = SearchApp()
     window.show()
+
     sys.exit(app.exec())
