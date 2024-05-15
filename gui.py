57ea2e7a64d0d941f87560f48dc0ffe312be80f0
Evgeny Loshkarev <evlosh@gmail.com>
2024-05-15 18:03:07 +0300

    ref

--- gui.py
+++ gui.py
@@ -3,11 +3,11 @@ import subprocess
 import sys
 from functools import partial
 
-from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QGuiApplication, QKeyEvent
+from PyQt6.QtCore import Qt, QTimer, pyqtSignal, QThread
+from PyQt6.QtGui import QDragEnterEvent, QDragLeaveEvent, QDropEvent, QGuiApplication, QKeyEvent
 from PyQt6.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
                              QLineEdit, QPushButton, QSpacerItem, QVBoxLayout,
-                             QWidget)
+                             QWidget, QScrollArea)
 
 from catalog_mirgrate import CatalogMigrateThread
 from catalog_search import catalog_search_file
@@ -15,53 +15,73 @@ from catalog_update import CatalogUpdateThread
 from cfg import Cfg
 
 
-class Warning(QWidget):
-    def __init__(self, parent: QWidget):
+class SearchThread(QThread):
+    result = pyqtSignal(str)
+    finished = pyqtSignal()
+
+    def __init__(self, path: str, filename: str):
         super().__init__()
-        self.setWindowModality(Qt.WindowModality.ApplicationModal)
+        self.path = path
+        self.filename = filename
+        self.stop_flag = False
 
-        self.setFixedSize(320, 90)
-        self.setWindowTitle("Каталог недоступен")
+    def run(self):
+        self.stop_flag = False
+        name_a = self.filename.lower()
 
-        self.init_ui()
+        for root, dirs, files in os.walk(self.path):
+            for file in files:
 
-        main_window_pos = parent.pos()
-        main_window_rect = parent.geometry()
-        center_x = main_window_pos.x() + main_window_rect.width() // 2
-        center_y = main_window_pos.y() + main_window_rect.height() // 2
-        self.move(center_x - self.width() // 2, center_y - self.height() // 2)
+                name_b = file.lower()
 
-    def init_ui(self):
-        main_layout = QVBoxLayout()
-        main_layout.setContentsMargins(15, 5, 15, 0)
-        main_layout.setSpacing(0)
-        self.setLayout(main_layout)
+                if name_a in name_b or name_a == name_b:
+                    self.result.emit(os.path.join(root, file))
 
-        lbl = QLabel("Каталог недоступен.\nНажмите кнопку \"Обзор\" и укажите каталог")
-        main_layout.addWidget(lbl)
+                elif self.stop_flag:
+                    self.finished.emit()
+                    return
 
-        btn = QPushButton("Понятно", self)
-        btn.setFixedWidth(100)
-        btn.clicked.connect(self.deleteLater)
-        main_layout.addWidget(btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.finished.emit()
 
+
+class DraggableLabel(QLabel):
+    path_selected = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setText("Перетяните сюда папку для поиска")
+        self.setAcceptDrops(True)
+        self.setStyleSheet("border: 2px dashed gray; padding: 20px; border-radius: 5px;")
+        self.setWordWrap(True)
+
+    def dragEnterEvent(self, a0: QDragEnterEvent | None) -> None:
+        if a0.mimeData().hasUrls():
+            a0.accept()
+        else:
+            a0.ignore()
+        return super().dragEnterEvent(a0)
+    
+    def dropEvent(self, a0: QDropEvent | None) -> None:
+        path = a0.mimeData().urls()[0].toLocalFile()
+        self.setText(path)
+        self.path_selected.emit(path)
+        self.setStyleSheet("border: none;")
+        return super().dropEvent(a0)
 
 
 class SearchApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.path = None
+        self.search_thread: SearchThread = None
 
         self.setWindowTitle(Cfg.app_name)
         self.base_w, self.base_h = 290, 210
+        self.temp_h = self.base_h
         self.setFixedSize(self.base_w, self.base_h)
         self.init_ui()
         self.setFocus()
         self.center()
-
-        after = QTimer(self)
-        after.setSingleShot(True)
-        after.timeout.connect(self.error_check)
-        after.start(300)
 
     def init_ui(self):
         self.base_layout = QVBoxLayout()
@@ -78,46 +98,31 @@ class SearchApp(QWidget):
         self.fixed_layout.setSpacing(0)
         self.fixed_widget.setLayout(self.fixed_layout)
 
-        # BROWSE CATALOG PATH
-        self.browse_cat_layout = QHBoxLayout()
-        self.browse_cat_layout.setContentsMargins(0, 0, 0, 0)
-        self.browse_cat_layout.setSpacing(0)
-        self.fixed_layout.addLayout(self.browse_cat_layout)
+        # DRAGABLE EVENT
 
-        self.browse_btn = QPushButton("Обзор", self)
-        self.browse_btn.setFixedWidth(70)
-        self.browse_btn.clicked.connect(self.choose_catalog)
-        self.browse_cat_layout.addWidget(self.browse_btn)
-
-        self.browse_cat_layout.addSpacerItem(QSpacerItem(10, 0))
-
-        self.browse_lbl = QLabel(Cfg.images_dir)
-        self.browse_lbl.setWordWrap(True)
-        self.set_browse_lbl_h()
-        self.browse_cat_layout.addWidget(self.browse_lbl)
-
-        # UPDATE CATALOG JSON
-        self.update_btn = QPushButton("Обновить каталог")
-        self.update_btn.clicked.connect(self.update_btn_cmd)
-        self.update_btn.setFixedWidth(150)
-        self.fixed_layout.addWidget(self.update_btn)
+        self.fixed_layout.addSpacerItem(QSpacerItem(0, 10))
+        self.dragable = DraggableLabel()
+        self.dragable.setFixedSize(self.base_w - 15, 100)
+        self.dragable.path_selected.connect(self.set_path)
+        self.fixed_layout.addWidget(self.dragable, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.fixed_layout.addSpacerItem(QSpacerItem(0, 10))
 
         # SEARCH INPUT
         self.input_text = QLineEdit()
-        self.input_text.setFixedHeight(30)
-        self.input_text.setStyleSheet(
-            f"""
-            padding-left: 5px;
-            border-radius: 5px;
-            """)
-        self.input_text.setPlaceholderText("Вставьте артикул или ссылку")
-        self.input_text.mouseReleaseEvent = self.select_all_text
+        self.input_text.setFixedSize(self.base_w - 10, 30)
+        self.input_text.setStyleSheet("padding-left: 5px; border-radius: 5px;")
+        self.input_text.setPlaceholderText("Вставьте имя файла")
         self.fixed_layout.addWidget(self.input_text)
 
-        # SEARCH BUTTON
+        self.fixed_layout.addSpacerItem(QSpacerItem(0, 10))
+
         self.search_button = QPushButton("Поиск")
-        self.search_button.clicked.connect(partial(self.btn_search_cmd))
-        self.fixed_layout.addWidget(self.search_button)
+        self.search_button.setFixedWidth(self.base_w // 2)
+        self.search_button.clicked.connect(self.btn_search_cmd)
+        self.fixed_layout.addWidget(self.search_button, alignment=Qt.AlignmentFlag.AlignCenter)
+
+
+        self.fixed_layout.addStretch()
         
         self.btns_widget = QWidget()
         self.base_layout.addWidget(self.btns_widget)
@@ -125,99 +130,61 @@ class SearchApp(QWidget):
         self.btns_layout = QVBoxLayout()
         self.btns_widget.setLayout(self.btns_layout)
 
-        self.base_layout.addStretch()
+        # self.base_layout.addStretch()
+    
+    def set_path(self, path: str):
+        self.path = path
 
     def keyPressEvent(self, a0: QKeyEvent | None) -> None:
         if a0.key() in (Qt.Key.Key_Enter, Qt.Key.Key_Return):
             self.btn_search_cmd()
         return super().keyPressEvent(a0)
 
-    def select_all_text(self, e):
-        if not self.input_text.hasFocus():
-            self.input_text.selectAll()
-
-    def error_check(self):
-        if Cfg.first_load:
-            self.warning()
-
-    def choose_catalog(self):
-        new_dir = QFileDialog.getExistingDirectory(self)
-
-        if new_dir:
-            old_dir = Cfg.images_dir
-            Cfg.images_dir = new_dir
-            Cfg.first_load = False
-            Cfg.write_cfg_json_file()
-
-            self.migrate_thread = CatalogMigrateThread(old_dir, new_dir)
-            self.migrate_thread.finished.connect(self.finalize_choose_catalog)
-            self.migrate_thread.start()
-
-    def finalize_choose_catalog(self):
-        self.gui_switch(setDisabled=False)
-        self.browse_lbl.setText(Cfg.images_dir)
-        self.set_browse_lbl_h()
-
-    def gui_switch(self, setDisabled: bool):
-        self.remove_article_btns()
-        for i in (self.update_btn, self.input_text, self.search_button):
-            i.setDisabled(setDisabled)
-
     def remove_article_btns(self):
         for i in reversed(range(self.btns_layout.count())):
             self.btns_layout.itemAt(i).widget().close()
 
-    def update_btn_cmd(self):
-        self.update_btn.setText("Подождите...")
-        self.t1 = CatalogUpdateThread()
-        self.t1.finished.connect(self.finalize_update_btn_cmd)
-        self.t1.start()
-
-    def finalize_update_btn_cmd(self):
-        self.update_btn.setText("Обновить базу данных")
-    
-    def warning(self):
-        self.gui_switch(setDisabled=True)
-        self.win = Warning(self)
-        self.win.show()
-
     def btn_search_cmd(self):
-        if not os.path.exists(Cfg.images_dir):
-            self.warning()
-            return
-
+        self.temp_h = self.base_h
         self.remove_article_btns()
         self.setFocus()
-        text: str = self.input_text.text()
-        
-        if "miuz.ru" in text:
-            text = text.strip("/").split("/")[-1]
 
-        if not text or len(text) < 5:
-            lbl = QLabel ("Не найдено")
+        if self.search_thread and self.search_thread.isRunning():
+            self.search_thread.stop_flag = True
+
+        if not self.path or not os.path.exists(self.path):
+            lbl = QLabel ("Укажите место поиска")
             self.btns_layout.addWidget(lbl)
             self.setFixedSize(self.base_w, self.base_h + 20)
             return
 
-        res: dict = catalog_search_file(text)
+        text: str = self.input_text.text()
 
-        if res:
-            advanved_size = 0
-            for name, src in res:
-                btn = QPushButton(name, self)
-                btn.clicked.connect(partial(self.article_btn_cmd, src))
-                btn.setStyleSheet("text-align:left")
-                self.btns_layout.addWidget(btn)
-                advanved_size += btn.height() + 20
-
-            
-            self.setFixedSize(self.base_w, self.base_h + advanved_size)
-
-        elif not res:
-            lbl = QLabel ("Не найдено")
+        if not text:
+            lbl = QLabel ("Введите текст")
             self.btns_layout.addWidget(lbl)
             self.setFixedSize(self.base_w, self.base_h + 20)
+            return
+        
+        self.search_thread = SearchThread(self.path, text)
+        self.search_thread.result.connect(self.add_article_btn)
+        self.search_thread.finished.connect(self.finish_search)
+        self.search_button.setText("Ищу...")
+        self.search_thread.start()
 
+    def finish_search(self):
+        self.search_button.setText("Поиск")
+    
+    def add_article_btn(self, path):
+        filename = os.path.basename(path)
+
+        btn = QPushButton(filename, self)
+        btn.clicked.connect(partial(self.article_btn_cmd, path))
+        btn.setStyleSheet("text-align:left")
+        self.btns_layout.addWidget(btn)
+
+        self.temp_h += 35
+        self.setFixedSize(self.base_w, self.temp_h)
 
     def article_btn_cmd(self, path: str):
         if os.path.exists(path):
@@ -232,13 +199,9 @@ class SearchApp(QWidget):
         window_geometry.moveCenter(screen_geometry.center())
         self.move(window_geometry.topLeft())
 
-    def set_browse_lbl_h(self):
-        lbl_h = 30
-        num_lines = self.browse_lbl.text().count('\n') + 1
-        self.browse_lbl.setFixedHeight(lbl_h * num_lines)
     
 if __name__ == "__main__":
-    Cfg.check_files()
+    # Cfg.check_files()
 
     if os.path.exists("lib"): 
         #lib folder appears when we pack this project to .app with py2app
